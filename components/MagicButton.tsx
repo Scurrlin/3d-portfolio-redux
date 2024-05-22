51e815382f135a742faac4d8f5c3c8b881272949
Sean Currlin <sean.currlin@gmail.com>
2024-05-22 17:09:55 -0400

    revert magic button changes

--- components/MagicButton.tsx
+++ components/MagicButton.tsx
@@ -1,5 +1,13 @@
 import React from "react";
 
+/**
+ *  UI: border magic from tailwind css btns
+ *  Link: https://ui.aceternity.com/components/tailwindcss-buttons
+ *
+ *  change border radius to rounded-lg
+ *  add margin of md:mt-10
+ *  remove focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50
+ */
 const MagicButton = ({
   title,
   icon,
@@ -22,7 +30,8 @@ const MagicButton = ({
 
       {/* remove px-3 py-1, add px-5 gap-2 */}
       <span
-        className={`inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg bg-slate-950 px-7 text-sm font-medium text-white backdrop-blur-3xl ${otherClasses}`}
+        className={`inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg
+             bg-slate-950 px-7 text-sm font-medium text-white backdrop-blur-3xl gap-2 ${otherClasses}`}
       >
         {position === "left" && icon}
         {title}
