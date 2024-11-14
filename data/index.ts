77aaacbc1722c091f60b88703db706f6f3b43341
Sean Currlin <sean.currlin@gmail.com>
2024-11-14 09:29:54 -0500

    update resume link

--- data/index.ts
+++ data/index.ts
@@ -2,7 +2,7 @@ export const navItems = [
     { name: "About", link: "#about" },
     { name: "Projects", link: "#projects" },
     { name: "Contact", link: "#contact" },
-    { name: "Resume", link: "https://drive.google.com/file/d/1II0Ku5Z7g5VqgbhuPM6NvbbK9SkWbxvo/view?usp=sharing" }
+    { name: "Resume", link: "https://drive.google.com/file/d/1g51G0gBMBYr6JHke3mdOhxGwPejvZnQc/view?usp=sharing" }
   ];
   
   export const gridItems = [
